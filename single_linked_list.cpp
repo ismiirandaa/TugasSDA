9c5950a1bdc6457576131b5ef8c2639bf8c87b2f
ismiirandaa <99125540+ismiirandaa@users.noreply.github.com>
2022-03-08 00:03:47 +0700

    Update single_linked_list.cpp

--- single_linked_list.cpp
+++ single_linked_list.cpp
@@ -1,41 +1,41 @@
-class Node(object):
-def __init__(self, data):
+class Node(object);{
+def __init__(self, data);
         self.data = data
-        self.nextNode = None
-class SinglyLinkedList(object):
-def __init__(self):
+        self.nextNode = None }
+class SinglyLinkedList(object); {
+def __init__(self);
         self.head = None
         self.size = 0
-def insertAtEnd(self, data):
+def insertAtEnd(self, data);
         newNode = Node(data)
-        self.size += 1
-if self.head is None:
+        self.size += 1 }
+if self.head is None;{
             self.head = newNode
-        else:
+        else;
             currentNode = self.head
             while currentNode.nextNode is not None:
                 currentNode = currentNode.nextNode
         
-            currentNode.nextNode = newNode
-def insertAtStart(self, data):
+            currentNode.nextNode = newNode }
+def insertAtStart(self, data); {
         newNode = Node(data)
-        self.size += 1
-if self.head is None:
+        self.size += 1 }
+if self.head is None;{
             self.head = newNode
         else:
             newNode.nextNode = self.head
-            self.head = newNode
+            self.head = newNode }
         
-  def removeAtStart(self):
+  def removeAtStart(self);
         currentNode = self.head
 if currentNode is not None:
             self.size -= 1
             self.head = currentNode.nextNode
         else:
             print("Linked list is empty")
-def removeAtEnd(self):
+def removeAtEnd(self);
         currentNode = self.head
-if currentNode is None:
+if currentNode is None; {
             print("linked List is empty")
         else:
             while currentNode.nextNode is not None:
@@ -43,13 +43,13 @@ if currentNode is None:
                 currentNode = currentNode.nextNode
         
             prevNode.nextNode = None
-            self.size -= 1
-def insertAtIndex(self, index, data):
+            self.size -= 1 }
+def insertAtIndex(self, index, data); {
         newNode = Node(data)
         self.size += 1
 currentNode = self.head
-        counter = 0
-if currentNode is None:
+        counter = 0 }
+if currentNode is None{
             self.head = newNode
         else:
             while counter < index:
@@ -62,13 +62,13 @@ if currentNode is None:
 def removeAtIndex(self, index):
         currentNode = self.head
         counter = 0
-while counter < index:
+while counter < index;
             counter += 1
             prevNode = currentNode
             currentNode = currentNode.nextNode
 prevNode.nextNode = currentNode.nextNode 
         self.size -= 1
-def removeData(self, data):
+def removeData(self, data);
         currentNode = self.head
 while currentNode.data is not data:
             prevNode = currentNode
@@ -78,13 +78,13 @@ if currentNode is self.head:
         else:
             prevNode.nextNode = currentNode.nextNode
         self.size -= 1
-def traverse(self):
+def traverse(self);
         currentNode = self.head
 if currentNode is None:
-            print("Linkedlist is empty")
+            print("Linkedlist is empty");
         else:
-            print(currentNode.data)
+            print(currentNode.data);
             while currentNode.nextNode is not None:
                 currentNode = currentNode.nextNode
-                print(currentNode.data)
-singly = SinglyLinkedList()
+                print(currentNode.data);
+singly = SinglyLinkedList();
